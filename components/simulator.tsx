be492c21f9f82423e68319531c1070891807668c
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-04-20 15:16:49 -0300

    remove radix tabs

--- components/simulator.tsx
+++ components/simulator.tsx
@@ -1,22 +1,9 @@
 import { Converter } from "./converter"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
 
 export function Simulator() {
   return (
     <div className="dark:bg-dark h-96 w-80 rounded-lg border border-border bg-slate-50 dark:bg-slate-950">
-      <Tabs defaultValue="enviar" className="p-1">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="enviar">Enviar</TabsTrigger>
-          <TabsTrigger value="receber">Receber</TabsTrigger>
-        </TabsList>
-
-        <TabsContent value="enviar">
-          <Converter tab="enviar" />
-        </TabsContent>
-        <TabsContent value="receber">
-          <Converter tab="receber" />
-        </TabsContent>
-      </Tabs>
+      <Converter tab="enivar" />
     </div>
   )
 }
